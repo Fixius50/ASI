efa2d5cdcc598a713b80896371ec1237d04a4883
Fixius50 <robertomonederoalonso0@gmail.com>
2025-12-04 22:28:26 +0100

    df

--- RenombrarImagenes.java
+++ RenombrarImagenes.java
@@ -7,8 +7,8 @@ public class RenombrarImagenes {
 
     public static void main(String[] args) {
         // 1. Definir la ruta de la carpeta
-        
-        String rutaCarpeta = "jetson_nano";
+
+        String rutaCarpeta = "comparacion_modelos/media";
         Path directorio = Paths.get(rutaCarpeta);
 
         // Validar que la carpeta existe
@@ -36,16 +36,18 @@ public class RenombrarImagenes {
                 String nuevoNombreArchivo = contador + ".jpg";
                 Path rutaDestino = directorio.resolve(nuevoNombreArchivo);
 
-                // Evitar sobreescribir si el archivo ya se llama "1.jpg" (por si corres el script 2 veces)
+                // Evitar sobreescribir si el archivo ya se llama "1.jpg" (por si corres el
+                // script 2 veces)
                 if (archivoOriginal.getFileName().toString().equals(nuevoNombreArchivo)) {
-                    System.out.println("El archivo " + nuevoNombreArchivo + " ya tiene el nombre correcto. Saltando...");
+                    System.out
+                            .println("El archivo " + nuevoNombreArchivo + " ya tiene el nombre correcto. Saltando...");
                     contador++;
                     continue;
                 }
 
                 // Ejecutar el cambio de nombre
                 Files.move(archivoOriginal, rutaDestino, StandardCopyOption.REPLACE_EXISTING);
-                
+
                 System.out.println("Renombrado: " + archivoOriginal.getFileName() + " -> " + nuevoNombreArchivo);
                 contador++;
             }
